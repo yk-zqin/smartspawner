2af2c0db1d3849e4f426d27db865b7da7513fc45
Dũng <99890979+maiminhdung@users.noreply.github.com>
2025-05-21 20:28:49 +0700

    Add RedProtect support

--- core/src/main/java/github/nighter/smartspawner/hooks/protections/CheckOpenMenu.java
+++ core/src/main/java/github/nighter/smartspawner/hooks/protections/CheckOpenMenu.java
@@ -1,31 +1,27 @@
 package github.nighter.smartspawner.hooks.protections;
 
-import java.util.UUID;
-
+import github.nighter.smartspawner.SmartSpawner;
 import github.nighter.smartspawner.hooks.protections.api.*;
-
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
-
-import github.nighter.smartspawner.SmartSpawner;
 
 public class CheckOpenMenu {
     // Check if player can open a menu
     public static boolean CanPlayerOpenMenu(@NotNull final Player player, @NotNull Location location) {
 
-        if(player.isOp() || player.hasPermission("*")) return true;
+        if (player.isOp() || player.hasPermission("*")) return true;
 
-        if (SmartSpawner.hasGriefPrevention && !GriefPrevention.canPlayerOpenMenuOnClaim(player, location)) return false;
+        if (SmartSpawner.hasGriefPrevention && !GriefPrevention.canPlayerOpenMenuOnClaim(player, location))
+            return false;
         if (SmartSpawner.hasWorldGuard && !WorldGuard.canPlayerInteractInRegion(player, location)) return false;
         if (SmartSpawner.hasLands && !Lands.CanPlayerInteractContainer(player, location)) return false;
         if (SmartSpawner.hasTowny && !Towny.canPlayerInteractSpawner(player, location)) return false;
         if (SmartSpawner.hasSuperiorSkyblock2 && SuperiorSkyblock2.canPlayerOpenMenu(player, location)) return false;
         if (SmartSpawner.hasBentoBox && !BentoBoxAPI.canPlayerOpenMenu(player, location)) return false;
-        if (SmartSpawner.hasSimpleClaimSystem && !SimpleClaimSystem.canPlayerOpenMenuOnClaim(player, location)) return false;
-
-        return true;
+        if (SmartSpawner.hasSimpleClaimSystem && !SimpleClaimSystem.canPlayerOpenMenuOnClaim(player, location))
+            return false;
+        return !SmartSpawner.hasRedProtect || RedProtectAPI.canPlayerOpenMenuOnClaim(player, location);
     }
 
 }
