b2a00cdfcabfbe39f62a8e7b7da9c301c9deffea
Alessandro <alessandroc160707@gmail.com>
2025-08-20 12:31:41 +0200

    Fixed BentoBox hook

--- core/src/main/java/github/nighter/smartspawner/hooks/protections/api/BentoBoxAPI.java
+++ core/src/main/java/github/nighter/smartspawner/hooks/protections/api/BentoBoxAPI.java
@@ -12,14 +12,14 @@ import java.util.UUID;
 
 public class BentoBoxAPI {
     public static boolean canPlayerStackBlock(@NotNull Player player, @NotNull Location location) {
-
+        if(BentoBox.getInstance().getIslandsManager().getIslandAt(location).isEmpty()) return true;
         return BentoBox.getInstance().getIslandsManager().getIslandAt(location).
                 map(island -> island.isAllowed(User.getInstance(player.getUniqueId()), Flags.PLACE_BLOCKS)).
                 orElse(Flags.PLACE_BLOCKS.isSetForWorld(location.getWorld()));
     }
 
     public static boolean canPlayerOpenMenu(@NotNull Player player, @NotNull Location location) {
-
+        if(BentoBox.getInstance().getIslandsManager().getIslandAt(location).isEmpty()) return true;
         return BentoBox.getInstance().getIslandsManager().getIslandAt(location).
                 map(island -> island.isAllowed(User.getInstance(player.getUniqueId()), Flags.CONTAINER)).
                 orElse(Flags.CONTAINER.isSetForWorld(location.getWorld()));
